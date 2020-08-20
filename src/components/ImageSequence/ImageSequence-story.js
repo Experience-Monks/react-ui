b8778033a521aceb3aea2f1c99ff659201d5261e
Artem Leznikov <artemleznikov@gmail.com>
2020-08-19 21:40:39 -0400

    feature(ImageSequence): add image loading logic, tooltips and positioning

--- src/components/ImageSequence/ImageSequence-story.js
+++ src/components/ImageSequence/ImageSequence-story.js
@@ -10,7 +10,32 @@ const props = {
       return `https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/05-flip-for-nc/${index
         .toString()
         .padStart(4, '0')}.jpg`;
-    })
+    }),
+  tooltips: [
+    {
+      percentPostionX: 25,
+      percentPositionY: 25,
+      percentVisibleStart: 20,
+      percentVisibleEnd: 45,
+      content: 'I am tooltip one!'
+    },
+    {
+      percentPostionX: 50,
+      percentPositionY: 50,
+      percentVisibleStart: 40,
+      percentVisibleEnd: 65,
+      content: 'I am tooltip two!'
+    },
+    {
+      percentPostionX: 75,
+      percentPositionY: 75,
+      percentVisibleStart: 60,
+      percentVisibleEnd: 85,
+      content: 'I am tooltip three!'
+    }
+  ],
+  percentDrawOffsetX: 100,
+  percentDrawOffsetY: 50
 };
 
 storiesOf('ImageSequence', module).addWithJSX('Default', () => <Component {...props} />);
--- src/components/ImageSequence/ImageSequence.js
+++ src/components/ImageSequence/ImageSequence.js
@@ -1,27 +1,32 @@
-import React, { memo, useLayoutEffect, useRef, useCallback } from 'react';
+import React, { memo, useLayoutEffect, useRef, useCallback, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import checkProps from '@jam3/react-check-extra-props';
 
 import './ImageSequence.css';
 
-import { clamp, getViewportHeight, ctxDrawImageCover } from './utils';
+import { clamp, getViewportHeight, drawImageCoverDimensions, isImageLoaded } from './utils';
 
-// ratio: 0 - 1 (0.25)
-// percentage: 0 - 100 (25%)
+const IMAGE_LOAD_GAP = 4;
 
-function ImageSequence({ className, imageUrls, heightMultiplier }) {
+// Terminology
+// Ratio: 0 - 1 (0.25)
+// Percentage: 0 - 100 (25%)
+
+function ImageSequence({ className, imageUrls, heightMultiplier, tooltips, percentDrawOffsetX, percentDrawOffsetY }) {
   const ImageSequenceElRef = useRef(null);
   const stickyContainerElRef = useRef(null);
   const canvasElRef = useRef(null);
+  const tooltipElsRef = useRef([]);
 
-  const activeImageOnLoad = useRef(null);
-  const scrolledRatio = useRef(0);
+  const activeImageOnLoadRef = useRef(null);
+  const scrolledRatioRef = useRef(0);
   const canvasContextRef = useRef(null);
-  const images = useRef(
-    imageUrls.map(imageUrl => {
+  const imagesRef = useRef(
+    imageUrls.map((imageUrl, index) => {
       const image = new Image();
-      image.src = imageUrl;
+      image.dataSrc = imageUrl;
+      if (index % IMAGE_LOAD_GAP === 0) image.src = image.dataSrc;
       return image;
     })
   );
@@ -33,32 +38,71 @@ function ImageSequence({ className, imageUrls, heightMultiplier }) {
     canvasElRef.current.height = height;
   }, []);
 
+  const setTooltipsVisibility = useCallback(() => {
+    tooltips.forEach(({ percentVisibleStart, percentVisibleEnd }, index) => {
+      const tooltipEl = tooltipElsRef.current[index];
+      const ratioVisibleStart = percentVisibleStart / 100;
+      const ratioVisibleEnd = percentVisibleEnd / 100;
+
+      if (scrolledRatioRef.current >= ratioVisibleStart && scrolledRatioRef.current <= ratioVisibleEnd) {
+        tooltipEl.style.visibility = 'visible';
+      } else {
+        tooltipEl.style.visibility = 'hidden';
+      }
+    });
+  }, []);
+
+  const setTooltipsPosition = useCallback(({ cx, cy, ih, iw, nw, nh }) => {
+    tooltips.forEach(({ percentPostionX, percentPositionY }, index) => {
+      const tooltipEl = tooltipElsRef.current[index];
+      const ratioPostionX = percentPostionX / 100;
+      const ratioPositionY = percentPositionY / 100;
+
+      tooltipEl.style.top = `${nh * ratioPositionY - (cy / ih) * nh}px`;
+      tooltipEl.style.left = `${nw * ratioPostionX - (cx / iw) * nw}px`;
+    });
+  }, []);
+
   const drawImage = useCallback(image => {
-    ctxDrawImageCover(
-      canvasContextRef.current,
-      image,
-      0,
-      0,
+    const ratioDrawOffsetX = percentDrawOffsetX / 100;
+    const ratioDrawOffsetY = percentDrawOffsetY / 100;
+
+    const dimensions = drawImageCoverDimensions(
+      image.width,
+      image.height,
       canvasElRef.current.width,
       canvasElRef.current.height,
-      0.5,
-      0.5
+      ratioDrawOffsetX,
+      ratioDrawOffsetY
     );
-  });
+
+    const { cx, cy, cw, ch, x, y, w, h } = dimensions;
+    canvasContextRef.current.drawImage(image, cx, cy, cw, ch, x, y, w, h);
+
+    setTooltipsPosition(dimensions);
+  }, []);
 
   const drawActiveImage = useCallback(() => {
-    if (activeImageOnLoad.current) activeImageOnLoad.current.onload = null;
+    if (activeImageOnLoadRef.current) activeImageOnLoadRef.current.onload = null;
 
-    const activeImageIndex = Math.round((images.current.length - 1) * scrolledRatio.current) || 0;
-    const activeImage = images.current[activeImageIndex];
+    const activeImageIndex = Math.round((imagesRef.current.length - 1) * scrolledRatioRef.current) || 0;
+    const activeImage = imagesRef.current[activeImageIndex];
 
-    if (activeImage.complete || activeImage.naturalWidth !== 0) {
+    if (isImageLoaded(activeImage)) {
       drawImage(activeImage);
     } else {
+      for (let substituteImageIndex = activeImageIndex - 1; substituteImageIndex >= 0; substituteImageIndex--) {
+        const substituteImage = imagesRef.current[substituteImageIndex];
+        if (isImageLoaded(substituteImage)) {
+          drawImage(substituteImage);
+          break;
+        }
+      }
+
+      activeImageOnLoadRef.current = activeImage;
       activeImage.onload = () => {
         drawImage(activeImage);
       };
-      activeImageOnLoad.current = activeImage;
     }
   }, []);
 
@@ -68,10 +112,11 @@ function ImageSequence({ className, imageUrls, heightMultiplier }) {
 
     function update() {
       const { top, height } = ImageSequenceElRef.current.getBoundingClientRect();
-      scrolledRatio.current = clamp((top / (height - getViewportHeight())) * -1, 0, 1);
+      scrolledRatioRef.current = clamp((top / (height - getViewportHeight())) * -1, 0, 1);
 
       setCanvasSize();
       drawActiveImage();
+      setTooltipsVisibility();
     }
 
     update();
@@ -84,16 +129,40 @@ function ImageSequence({ className, imageUrls, heightMultiplier }) {
     };
   }, []);
 
+  useEffect(() => {
+    // load remainder of the images
+    imagesRef.current.forEach(image => {
+      if (!image.src) image.src = image.dataSrc;
+    });
+  }, []);
+
   return (
     <div
       className={classnames('ImageSequence', className)}
       ref={ImageSequenceElRef}
       style={{
-        height: `${images.current.length * heightMultiplier}vh`
+        height: `${imagesRef.current.length * heightMultiplier}vh`
       }}
     >
       <div className="stickyContainer" ref={stickyContainerElRef}>
         <canvas className="canvas" ref={canvasElRef} />
+
+        {tooltips.map(({ percentPostionX, percentPositionY, content }, index) => {
+          return (
+            <button
+              key={index}
+              className="tooltip"
+              style={{
+                top: `${percentPositionY}%`,
+                left: `${percentPostionX}%`
+              }}
+              onClick={() => {
+                alert(content);
+              }}
+              ref={el => (tooltipElsRef.current[index] = el)}
+            />
+          );
+        })}
       </div>
     </div>
   );
@@ -102,12 +171,16 @@ function ImageSequence({ className, imageUrls, heightMultiplier }) {
 ImageSequence.propTypes = checkProps({
   className: PropTypes.string,
   imageUrls: PropTypes.arrayOf(PropTypes.string),
-  heightMultiplier: PropTypes.number
+  heightMultiplier: PropTypes.number,
+  percentDrawOffsetX: PropTypes.number,
+  percentDrawOffsetY: PropTypes.number
 });
 
 ImageSequence.defaultProps = {
   imageUrls: [],
-  heightMultiplier: 3
+  heightMultiplier: 3,
+  percentDrawOffsetX: 50,
+  percentDrawOffsetY: 50
 };
 
 export default memo(ImageSequence);
