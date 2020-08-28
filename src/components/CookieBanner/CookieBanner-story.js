ee9ca3c88201cbfd8b72dc78787630d904a00f8f
namniak <vnamnyak@gmail.com>
2020-08-28 12:49:49 -0400

    cookie banner improvements

--- src/components/CookieBanner/CookieBanner-story.js
+++ src/components/CookieBanner/CookieBanner-story.js
@@ -2,8 +2,20 @@ import React, { Fragment } from 'react';
 import { storiesOf } from '@storybook/react';
 import CookieBanner from './CookieBanner';
 
-storiesOf('CookieBanner', module).add('Default', () => (
-  <Fragment>
-    <CookieBanner />
-  </Fragment>
-));
+storiesOf('CookieBanner', module)
+  .add('Default', () => (
+    <Fragment>
+      <CookieBanner onAccept={() => console.log('accept')} onReject={() => console.log('reject')} />
+    </Fragment>
+  ))
+  .add('With children', () => (
+    <Fragment>
+      <CookieBanner onAccept={() => console.log('accept')} onReject={() => console.log('reject')}>
+        We use cookies on this website to improve your experience. Learn more on our{' '}
+        <a href="https://www.jam3.com/privacy" target="_blank'">
+          Privacy Policy
+        </a>
+        .
+      </CookieBanner>
+    </Fragment>
+  ));
